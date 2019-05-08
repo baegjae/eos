8f787b619aa5a6370a3eb16b3ebcc04766fc9e3b
Baegjae Sung <baegjae@gmail.com>
2019-05-08 11:54:17 +0900

    Modified on_incoming message to get trxs and latency

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -339,7 +339,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
 
 
          if( fc::time_point::now() - block->timestamp < fc::minutes(5) || (block->block_num() % 1000 == 0) ) {
-            ilog("Received block ${id}... #${n} @ ${t} signed by ${p} [trxs: ${count}, lib: ${lib}, conf: ${confs}, latency: ${latency} ms]",
+            ilog("Received block ${id}... #${n} @ ${t} signed by ${p} [trxs: ^${count}^, lib: ${lib}, conf: ${confs}, latency: !${latency}! ms]",
                  ("p",block->producer)("id",fc::variant(block->id()).as_string().substr(8,16))
                  ("n",block_header::num_from_id(block->id()))("t",block->timestamp)
                  ("count",block->transactions.size())("lib",chain.last_irreversible_block_num())("confs", block->confirmed)("latency", (fc::time_point::now() - block->timestamp).count()/1000 ) );
